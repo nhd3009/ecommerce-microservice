7864e0d21b3304064011060ee84b7800281acfc9
nhd3009 <conan675@live.com>
2026-01-22 14:55:15 +0700

    feat: implement basic kafka function in notification service

---
+++ order-service/src/main/java/com/nhd/order_service/config/OrderEventPublisher.java
@@ -0,0 +1,27 @@
+package com.nhd.order_service.config;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.nhd.order_service.dto.OrderNotificationEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderEventPublisher {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private static final String TOPIC = "order-notification-events";
+
+    public void publishOrderNotification(OrderNotificationEvent event) {
+        try {
+            kafkaTemplate.send(TOPIC, event);
+            log.info("[KAFKA PUBLISH] Sent OrderNotificationEvent for orderId {}", event.getOrderId());
+        } catch (Exception e) {
+            log.error("[KAFKA ERROR] Failed to send event for orderId {}: {}", event.getOrderId(), e.getMessage(), e);
+        }
+    }
+}
